ba2e447c6e4c03de635f1386aad4376cf74872a3
dishasaxena0415 <62932559+dishasaxena0415@users.noreply.github.com>
2020-03-31 14:25:11 +0530

    pattern 1 created

---
+++ star1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Star1
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+for(int i=0;i<n;i++)
+{
+for(int j=0;j<i;j++)
+{
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
